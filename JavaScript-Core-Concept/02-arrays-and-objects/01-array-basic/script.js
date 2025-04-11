44a42770cf5d75dea48ce9db229f56f433d21e7b
MD SOHEL <[200~sohelonlinefreelancer@gmail.com>
2025-04-11 20:16:05 +0200

    JavaScript Array Basic

---
+++ JavaScript-Core-Concept/02-arrays-and-objects/01-array-basic/script.js
@@ -0,0 +1,27 @@
+let city = ["Dhaka", "Sylhet", 'Dinajpor', 'Nilphamari', 'Gazipur'];
+
+console.log(city);
+console.log(city[0]); // Dhaka
+
+const numbers = [10, 15, 20, 30, 40, 50]
+const arrays = [true, 'Sohel', 25]
+
+console.log(numbers);
+console.log(arrays);
+
+let schoolName = [];
+schoolName[0] = 'Law';
+schoolName[1] = 'Business';
+schoolName[2] = 'Science';
+console.log(schoolName);
+
+// Array Constructor
+
+let europe = new Array('German', 'France', 'Denmark', 'Spain', 'Estonia');
+console.log(europe);
+
+// Access Value by index
+
+let german = europe[0];
+let spain = europe[3];
+console.log(`${german}\n${spain}\n${europe[4]}`);
